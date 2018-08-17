90f5049cc70b75327e0c1564b4a5722d3f2e0534
antons.cornijs <antons.cornijs@accenture.com>
2018-08-17 14:00:06 +0300

    Image download and Post creation functionality

--- controllers/mediaController.js
+++ controllers/mediaController.js
@@ -1,7 +1,29 @@
 import Logger from '../utils/logger';
+import AppError from '../errors/AppError';
 import * as MediaModel from '../models/MediaModel';
+import * as PostModel from '../models/PostModel';
 
 const logger = Logger('mediaController');
+
+const addPost = async (req, res) => {
+  logger.log('debug', `addPost: ${JSON.stringify(req.body)}`);
+  const { user } = req;
+  const { caption, contentId } = req.body;
+  const media = await MediaModel.getMediaById(contentId);
+  if (!media) {
+    throw new AppError('No media with such ID!', 400);
+  }
+  const { path } = media;
+  const post = await PostModel.save({
+    caption,
+    username: user.username,
+    media: {
+      path,
+      contentId,
+    },
+  });
+  res.status(200).send({ payload: { post } });
+};
 
 const uploadImage = async (req, res) => {
   logger.log('debug', 'Upload Image!');
@@ -24,4 +46,16 @@ const uploadImage = async (req, res) => {
   });
 };
 
-export { uploadImage };
+const getAllPosts = async (req, res) => {
+  logger.log('debug', `getAllPosts`);
+  const posts = await PostModel.getAllPosts();
+  res.status(200).send({ payload: posts });
+};
+
+const getPostById = async (req, res) => {
+  logger.log('debug', `getPostById ${JSON.stringify(req.params)}`);
+  const post = await PostModel.getPostsById(req.params.id);
+  res.status(200).send({ payload: post });
+};
+
+export { uploadImage, addPost, getAllPosts, getPostById };
